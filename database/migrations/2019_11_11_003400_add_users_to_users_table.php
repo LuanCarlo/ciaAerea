35f7c4a630ec0c0b559c2e01bc228feff2bffe7e
Luan Carlo Santos de Paulo <luancarlo.paulo@gmail.com>
2019-11-10 22:01:31 -0300

    master
    adicao de migration de inclusão de cliente

---
+++ database/migrations/2019_11_11_003400_add_users_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUsersToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->insert(
+            array(
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('admin123'),
+                'tipo' => 1
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
